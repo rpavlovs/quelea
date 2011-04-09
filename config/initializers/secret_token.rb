72eabe840094a3ed65a693a45c22ea84e6340f45
Roman Pavlovskyi <wilohvost@gmail.com>
2011-04-08 23:24:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quelea::Application.config.secret_token = 'e9e175eb8f25273e94fb58a08abccf71361401bdc94b48366e7be54bc7b6e34e04671f4e33c0f156a436ffadf89589ea28331e991ed6742d0a5b9571cd3659b7'
